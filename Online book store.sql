05795c908024071711bccf03c407bcffbf078948
Sergey Oshemkov <oshemkov@yandex.ru>
2021-03-04 01:15:51 +0300

    Update of the project.

--- Online book store.sql
+++ Online book store.sql
@@ -90,3 +90,18 @@ WHERE
   date_step_end IS NULL
 ORDER BY
   buy_id;
+
+
+  /*    */
+
+  SELECT
+      name_client
+  FROM author
+      INNER JOIN book USING(author_id)
+      INNER JOIN buy_book USING(book_id)
+      INNER JOIN buy USING(buy_id)
+      INNER JOIN client USING(client_id)
+  WHERE name_author = "Достоевский Ф.М.";
+
+
+  
