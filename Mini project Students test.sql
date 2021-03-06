f7b4e5e668cabb5ff04c4f9aea386a9d394f80db
Sergey Oshemkov <oshemkov@yandex.ru>
2021-03-06 10:09:39 +0300

    Mini project. Students test.

---
+++ Mini project Students test.sql
@@ -0,0 +1,27 @@
+/* Select students who passed the discipline "Basics of databases", indicate the
+ date of the attempt and the result. Display information in descending order of
+ test results. */
+
+SELECT name_subject,
+       COUNT(attempt_id) AS Количество,
+       ROUND(AVG(result), 2) AS Среднее
+FROM attempt
+    RIGHT JOIN subject ON attempt.subject_id = subject.subject_id
+GROUP BY
+    name_subject;
+
+/*  Select the number of attempts made by students in each discipline, as well
+as the average result of attempts, which should be rounded to 2 decimal places.
+The result of an attempt stands for the percentage of correct answers to test
+questions, which is filled in to the result column.  */
+
+SELECT name_subject,
+       COUNT(attempt_id) AS Количество,
+       ROUND(AVG(result), 2) AS Среднее
+FROM attempt
+    RIGHT JOIN subject ON attempt.subject_id = subject.subject_id
+GROUP BY
+    name_subject;
+
+
+/*   */
