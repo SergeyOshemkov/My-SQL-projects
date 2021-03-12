e7a017e734ebb0dbb419ea8cfd38d8f18b3119c8
Sergey Oshemkov <oshemkov@yandex.ru>
2021-03-12 15:17:46 +0300

    Update of the mini project.

---
+++ Mini project Enrollees.sql
@@ -0,0 +1,25 @@
+
+
+/*  In the table attempt to include a new attempt for the student Pavel Baranov
+in the discipline "The basics of DBs". Set the current date as the date of the
+attempt. */
+
+
+INSERT INTO attempt(
+                    student_id, 
+                    subject_id, 
+                    date_attempt
+                    )
+SELECT
+    student.student_id, 
+    subject.subject_id, NOW()
+FROM student 
+    INNER JOIN attempt USING(student_id)
+    INNER jOIN subject USING(subject_id) 
+WHERE 
+    name_subject = "Основы баз данных" 
+    AND 
+    name_student = "Баранов Павел";
+
+
+/*   */
